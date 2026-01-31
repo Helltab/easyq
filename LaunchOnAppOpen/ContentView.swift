2511e8ef42f4f191221b2f7265ea5c5960903ae4
Daniel Canedo <dnl.canedo@gmail.com>
2026-01-31 14:33:56 -0400

    Initial Commit

---
+++ LaunchOnAppOpen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LaunchOnAppOpen
+//
+//  Created by DanielSeq on 31/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LaunchOnAppOpen/LaunchOnAppOpenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchOnAppOpenApp.swift
+//  LaunchOnAppOpen
+//
+//  Created by DanielSeq on 31/1/26.
+//
+
+import SwiftUI
+
+@main
+struct LaunchOnAppOpenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
